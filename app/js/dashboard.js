9125d125f4e490dcfefd9bc068a1ec798d90a743
Shihab <shehabull@gmail.com>
2019-03-29 01:45:13 +0600

    dashborad template initial ui

---
+++ app/js/dashboard.js
@@ -0,0 +1,69 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  //---------- for hamburger menu ---------
+  $('.icon_menu').click(function(){
+    $('.sidebar').toggleClass('show-hide-sidebar');
+    $('.content').toggleClass('show-hide-content');
+    $('.logo').toggleClass('sm-logo');
+    $('.header__right').toggleClass('extend-header');
+  })
+
+  $('.collapse-menu ').click(function(){
+    $('.header__icons').toggleClass('header__icons--show');
+    $('.content').toggleClass('visiable-header-icon');
+    $('.sidebar').toggleClass('visiable-header-icon');
+  })
+
+
+  //---------- for icon ---------
+  feather.replace();
+
+  //----------- for Graphs ---------------------
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+}())
